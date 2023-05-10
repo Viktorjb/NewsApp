905854cc63e13717ee07f2d9e374b4c31338daa7
Viktorjb <vjagebrant@gmail.com>
2023-05-10 15:49:32 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Viktor on 2023-05-10.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
