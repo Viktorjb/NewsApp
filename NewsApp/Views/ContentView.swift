7a5ebe7b3e5525b231618a3bbf9e7ef5f861df0d
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2023-05-15 23:06:55 +0200

    admin login and user profile

--- NewsApp/Views/ContentView.swift
+++ NewsApp/Views/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                 })
                 .padding()
                 .offset(x: 150)
-                .navigationBarTitle("profile View")
+                .navigationBarTitle("profile Vie")
                 .fullScreenCover(isPresented: $isprofile) {
                     ProfileView()
                 }
