b209e7e5a69bd9dc46ebbea180e6f90ae90ebeee
Viktorjb <vjagebrant@gmail.com>
2023-05-19 00:38:40 +0200

    Submission popup

--- NewsApp/Views/AddArticleView.swift
+++ NewsApp/Views/AddArticleView.swift
@@ -12,14 +12,20 @@ import SwiftUI
 struct AddArticleView: View {
     @StateObject var viewModel = AddArticleViewModel()
     let lists = ArticleLists()
+    //For alert-popup
+    @State var showingAlert = false
     
     var body: some View {
             VStack{
                 TextEditor(text: $viewModel.titleContent).padding(30).background(Color(red: 240/255, green: 240/255, blue: 245/255))
                     .frame(height: 100)
                 TextEditor(text: $viewModel.textContent).padding(30).background(Color(red: 240/255, green: 240/255, blue: 245/255))
-                Button("Publish", action: {viewModel.requestArticle()}
-                )
+                Button("Publish", action: {//viewModel.requestArticle()
+                    showingAlert = true
+                }
+                ).alert(viewModel.alertMessage, isPresented: $showingAlert) {
+                    Button("OK", role: .cancel) { }
+                }
             }
             .background(Color(red: 240/255, green: 240/255, blue: 245/255))
         
